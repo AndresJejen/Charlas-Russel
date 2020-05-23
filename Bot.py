2a037e0c543835931b20017f4e850e176575683b
Andres <gajcam@gmail.com>
2020-05-22 19:09:17 -0500

    Aprendiendo Diccionarios

---
+++ Bot.py
@@ -0,0 +1,10 @@
+miDiccionario = { 
+                    "nombre": "Andres Jejen",
+                    "profesion": "Programador",
+                    "telefono": "123456",
+                    "sexo": "M" 
+                }
+
+print(miDiccionario["nombre"])
+print(miDiccionario["telefono"])
+
