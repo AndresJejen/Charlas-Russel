34ab96213becd0082456c2349bf7d499a40ba521
Andres <gajcam@gmail.com>
2020-05-22 19:42:25 -0500

    Mi Bot calcula deuda

--- Bot.py
+++ Bot.py
@@ -1,8 +1,11 @@
+from MibancoFuncion import consultarValorApagar
+
 Bot =   {
             "Hola": "Hola ¿Cómo estas hoy?",
             "Cuentame un chiste": "¿Qué hacian dos pollitos en frente de un asadero? --- viendo una pelicula de terror",
             "Cuentame otro": "¿Que hace un perro con un taladro? --- Taladrando",
-            "Chao": "Adios, espero verte mañana"
+            "Chao": "Adios, espero verte mañana",
+            "consultar deuda": "Ok Ingresa los valores",
         }
 
 mensaje = ""
@@ -11,5 +14,9 @@ while (mensaje != "Chao"):
     try:
         mensaje = input(" Tu: ")
         print(Bot[mensaje])
+
+        if (mensaje == "consultar deuda"):
+            consultarValorApagar()
+
     except:
         print("No entiendo tu pregunta -- repitemela por favor")
